801ef9c055bb7b8b52013c89de01a2c0b7a28333
vipindagar7 <codewithvipin@gmail.com>
2024-04-20 12:04:06 +0530

    update static in settings

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -132,11 +132,8 @@ USE_TZ = True
 
 STATIC_URL = "/static/"
 STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
-
-
-MEDIA_URL = '/media/'
-MEDIA_ROOT = BASE_DIR / 'media'
-
+STATICFILES_DIRS = (os.path.join(BASE_DIR, "static"),)
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
